876dba679d2c74c7279f28dda34da254346edeb9
RubaanxD <teckever6@hotmail.com>
2026-06-11 13:03:20 -0600

    feat: connect to SCSTelemetry shared memory

--- src/main/java/com/convoyos/ConvoyOS.java
+++ src/main/java/com/convoyos/ConvoyOS.java
@@ -7,15 +7,17 @@ public class ConvoyOS {
     public static void main(String[] args) {
 
         System.out.println("================================");
-        System.out.println("         ConvoyOS");
+        System.out.println("          ConvoyOS");
         System.out.println("================================");
 
         SharedMemoryReader reader =
                 new SharedMemoryReader();
 
+        boolean connected =
+                reader.connect();
+
         System.out.println(
-                "Telemetry Available: "
-                + reader.isAvailable());
+                "Conectado: " + connected);
 
     }
 
--- src/main/java/com/convoyos/memory/SharedMemoryReader.java
+++ src/main/java/com/convoyos/memory/SharedMemoryReader.java
@@ -1,12 +1,36 @@
 package com.convoyos.memory;
 
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+
 public class SharedMemoryReader {
 
-    public boolean isAvailable() {
+    private static final String MMF_NAME = "Local\\SCSTelemetry";
 
-        // Temporalmente simulado
+    public boolean connect() {
+
+        HANDLE handle = Kernel32.INSTANCE.OpenFileMapping(
+                Kernel32.FILE_MAP_READ,
+                false,
+                MMF_NAME);
+
+        if (handle == null) {
+            System.out.println("OpenFileMapping devolvió null");
+            return false;
+        }
+
+        long pointerValue = Pointer.nativeValue(handle.getPointer());
+
+        if (pointerValue == 0) {
+            System.out.println("Handle inválido");
+            return false;
+        }
+
+        System.out.println("Handle obtenido correctamente");
+
+        Kernel32.INSTANCE.CloseHandle(handle);
 
         return true;
     }
-
 }
